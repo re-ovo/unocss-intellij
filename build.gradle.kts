f4284d1df31c868b8e7b6839b021ad2fe1f303f2
re-ovo <qq1609403959@outlook.com>
2023-06-28 16:36:20 +0800

    build: update gralde intellij plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("java")
     id("org.jetbrains.kotlin.jvm") version "1.8.22"
-    id("org.jetbrains.intellij") version "1.14.1"
+    id("org.jetbrains.intellij") version "1.14.2"
 }
 
 group = "me.rerere"

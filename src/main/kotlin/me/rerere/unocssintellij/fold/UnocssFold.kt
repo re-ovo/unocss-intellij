2b575889a3edd3ff2bf97ff82f3bbba4396d08d9
re-ovo <qq1609403959@outlook.com>
2023-07-05 22:11:44 +0800

    fold by default

--- src/main/kotlin/me/rerere/unocssintellij/fold/UnocssFold.kt
+++ src/main/kotlin/me/rerere/unocssintellij/fold/UnocssFold.kt
@@ -1,22 +1,31 @@
 package me.rerere.unocssintellij.fold
 
 import com.intellij.lang.ASTNode
-import com.intellij.lang.folding.FoldingBuilder
 import com.intellij.lang.folding.FoldingBuilderEx
 import com.intellij.lang.folding.FoldingDescriptor
 import com.intellij.openapi.editor.Document
 import com.intellij.psi.PsiElement
+import com.intellij.psi.util.PsiTreeUtil
+import me.rerere.unocssintellij.lang.psi.UnocssClassValue
 
 class UnocssFold : FoldingBuilderEx() {
     override fun buildFoldRegions(root: PsiElement, document: Document, quick: Boolean): Array<FoldingDescriptor> {
-        TODO("Not yet implemented")
+        val descriptors = mutableListOf<FoldingDescriptor>()
+        val classValues = PsiTreeUtil.findChildrenOfType(root, UnocssClassValue::class.java)
+        if(classValues.isNotEmpty()) {
+            classValues.forEach {
+                descriptors.add(FoldingDescriptor(it, it.textRange))
+            }
+        }
+
+        return descriptors.toTypedArray()
     }
 
     override fun getPlaceholderText(node: ASTNode): String? {
-        TODO("Not yet implemented")
+        return "Unocss"
     }
 
     override fun isCollapsedByDefault(node: ASTNode): Boolean {
-        TODO("Not yet implemented")
+        return true
     }
 }

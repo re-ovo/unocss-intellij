dff1b5a89dc9489f40f3ba3adf4b26ae52f4cb51
re-ovo <qq1609403959@outlook.com>
2023-09-05 22:21:01 +0800

    fix: fixed duplicated theme completion

--- src/main/kotlin/me/rerere/unocssintellij/references/UnocssThemeConfigReference.kt
+++ src/main/kotlin/me/rerere/unocssintellij/references/UnocssThemeConfigReference.kt
@@ -1,14 +1,9 @@
 package me.rerere.unocssintellij.references
 
-import com.intellij.codeInsight.lookup.LookupElement
-import com.intellij.codeInsight.lookup.LookupElementBuilder
-import com.intellij.icons.AllIcons
 import com.intellij.lang.javascript.psi.JSObjectLiteralExpression
 import com.intellij.lang.javascript.psi.JSProperty
 import com.intellij.openapi.util.TextRange
 import com.intellij.psi.*
-import com.intellij.util.ui.ColorIcon
-import me.rerere.unocssintellij.util.parseHexColor
 
 open class UnocssThemeConfigReference(element: PsiElement, protected val textRange: TextRange) :
     PsiReferenceBase<PsiElement>(element, textRange),
@@ -40,50 +35,6 @@ open class UnocssThemeConfigReference(element: PsiElement, protected val textRan
         }
 
         return result.toTypedArray()
-    }
-
-    override fun getVariants(): Array<Any> {
-        val themeConfigValue = UnoConfigPsiHelper.findThemeConfig(element) ?: return emptyArray()
-        if (themeConfigValue !is JSObjectLiteralExpression) return emptyArray()
-
-        val variants = mutableListOf<LookupElement>()
-        // match path prefix
-        val objectPath = themeValue.split(".").dropLast(1)
-        val properties = if (objectPath.isEmpty()) {
-            themeConfigValue.properties
-        } else {
-            val parentObj = UnoConfigPsiHelper.findThemeConfigProperty(themeConfigValue, objectPath)
-                ?: return emptyArray()
-
-            val parentObjectValue = parentObj.value
-            if (parentObjectValue is JSObjectLiteralExpression) {
-                parentObjectValue.properties
-            } else {
-                emptyArray()
-            }
-        }
-        if (properties.isEmpty()) {
-            return emptyArray()
-        }
-
-        val prefixPath = objectPath.joinToString(".")
-        for (prop in properties) {
-            val propName = prop.name ?: continue
-            val propValue = prop.value ?: continue
-
-            val lookupString = if (prefixPath.isBlank()) propName else "$prefixPath.${propName}"
-            val color = parseHexColor(propValue.text.trim('\'', '"'))
-
-            variants.add(
-                LookupElementBuilder
-                    .create(prop, lookupString)
-                    .withTypeText(themeConfigValue.containingFile.name)
-                    .withTailText(computeTailText(prop), true)
-                    .withIcon(if (color != null) ColorIcon(16, color) else AllIcons.Nodes.Property)
-            )
-        }
-
-        return variants.toTypedArray()
     }
 
     protected fun computeTailText(property: JSProperty): String {

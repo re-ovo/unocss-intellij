91d900ff2285302581c32499e6355815b9a36bf2
AiHMin <liusong00@hotmail.com>
2023-09-05 10:27:59 +0800

    fix: narrow CSS completion only for `@apply` and `--at-apply`

--- src/main/kotlin/me/rerere/unocssintellij/completion/UnocssCSSCompletionContributor.kt
+++ src/main/kotlin/me/rerere/unocssintellij/completion/UnocssCSSCompletionContributor.kt
@@ -5,22 +5,58 @@ import com.intellij.codeInsight.completion.CompletionParameters
 import com.intellij.codeInsight.completion.CompletionResultSet
 import com.intellij.codeInsight.completion.CompletionType
 import com.intellij.patterns.PlatformPatterns
+import com.intellij.psi.PsiElement
+import com.intellij.psi.css.impl.CssAtRuleImpl
+import com.intellij.psi.css.impl.CssDeclarationImpl
 import com.intellij.psi.css.impl.CssElementTypes
+import com.intellij.psi.filters.ElementFilter
+import com.intellij.psi.filters.position.FilterPattern
+import com.intellij.psi.util.PsiTreeUtil
 import com.intellij.psi.util.elementType
 
 class UnocssCSSCompletionContributor : CompletionContributor() {
     init {
         extend(
             CompletionType.BASIC,
-            PlatformPatterns.psiElement(CssElementTypes.CSS_IDENT),
+            PlatformPatterns.psiElement(CssElementTypes.CSS_IDENT)
+                .and(FilterPattern(UnocssCssCompletionFilter)),
             UnocssCSSTermListCompletionProvider
         )
         extend(
             CompletionType.BASIC,
-            PlatformPatterns.psiElement(CssElementTypes.CSS_STRING_TOKEN),
+            PlatformPatterns.psiElement(CssElementTypes.CSS_STRING_TOKEN)
+                .and(FilterPattern(UnocssCssCompletionFilter)),
             UnocssCSSTermListCompletionProvider
         )
     }
+}
+
+object UnocssCssCompletionFilter : ElementFilter {
+
+    /**
+     * Unocss completion in css scope only support `@apply` and `--at-apply` value
+     *
+     * other directives like `@screen` and `theme()` will be provided by ReferenceContributor
+     */
+    override fun isAcceptable(element: Any, context: PsiElement?): Boolean {
+        val psiElement = element as PsiElement
+        if (!psiElement.isValid) {
+            return false
+        }
+
+        val cssDeclaration: PsiElement = PsiTreeUtil.getParentOfType(
+            psiElement,
+            // for --at-apply
+            CssDeclarationImpl::class.java,
+            // for @apply
+            CssAtRuleImpl::class.java
+        ) ?: return false
+
+        val propKey = cssDeclaration.firstChild
+        return propKey.text == "--at-apply" || propKey.text == "@apply"
+    }
+
+    override fun isClassAcceptable(hintClass: Class<*>?): Boolean = true
 }
 
 object UnocssCSSTermListCompletionProvider : UnocssCompletionProvider() {

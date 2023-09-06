b577511efcdaec4ed9bf4681042bd1201572112f
re-ovo <qq1609403959@outlook.com>
2023-09-06 10:11:06 +0800

    fix: highlight pattern match

--- src/main/kotlin/me/rerere/unocssintellij/highlighting/UnocssAttributeExternalAnnotator.kt
+++ src/main/kotlin/me/rerere/unocssintellij/highlighting/UnocssAttributeExternalAnnotator.kt
@@ -10,13 +10,18 @@ import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.TextRange
 import com.intellij.openapi.util.text.StringUtil
 import com.intellij.psi.PsiDocumentManager
+import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
+import com.intellij.psi.css.impl.CssElementTypes
+import com.intellij.psi.util.elementType
+import com.intellij.psi.xml.XmlElementType
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withTimeoutOrNull
 import me.rerere.unocssintellij.UnocssService
 import me.rerere.unocssintellij.settings.UnocssSettingsState
 import me.rerere.unocssintellij.util.MatchedPosition
 import me.rerere.unocssintellij.util.getMatchedPositions
+import me.rerere.unocssintellij.util.isJsString
 
 data class UnocssInitInfo(
     val project: Project,
@@ -74,10 +79,25 @@ class UnocssAttributeExternalAnnotator : ExternalAnnotator<UnocssInitInfo, Unocs
 
     override fun apply(file: PsiFile, annotationResult: UnocssAnnotationResult?, holder: AnnotationHolder) {
         annotationResult?.annotations?.forEach {
-            holder.newSilentAnnotation(HighlightSeverity.TEXT_ATTRIBUTES)
-                .range(TextRange.create(it.start, it.end))
-                .textAttributes(DefaultLanguageHighlighterColors.HIGHLIGHTED_REFERENCE)
-                .create()
+            val element = file.findElementAt(it.start) ?: return@forEach
+            if (isValidElement(element)) {
+                holder.newSilentAnnotation(HighlightSeverity.TEXT_ATTRIBUTES)
+                    .range(TextRange.create(it.start, it.end))
+                    .textAttributes(DefaultLanguageHighlighterColors.HIGHLIGHTED_REFERENCE)
+                    .create()
+            }
         }
     }
+}
+
+private val VALID_ELEMENT_TYPES = setOf(
+    XmlElementType.XML_ATTRIBUTE_VALUE_TOKEN,
+    XmlElementType.XML_NAME,
+    CssElementTypes.CSS_STRING_TOKEN,
+    CssElementTypes.CSS_IDENT,
+)
+
+private fun isValidElement(element: PsiElement): Boolean {
+    val elementType = element.elementType
+    return elementType in VALID_ELEMENT_TYPES || isJsString(element)
 }
--- src/main/kotlin/me/rerere/unocssintellij/util/PsiHelper.kt
+++ src/main/kotlin/me/rerere/unocssintellij/util/PsiHelper.kt
@@ -1,5 +1,9 @@
 package me.rerere.unocssintellij.util
 
+import com.intellij.lang.javascript.psi.JSLiteralExpression
+import com.intellij.patterns.PlatformPatterns
+import com.intellij.psi.PsiElement
+import com.intellij.psi.impl.source.tree.LeafPsiElement
 import com.intellij.xml.util.HtmlUtil
 
 val classTagNameVariants = setOf(
@@ -23,3 +27,9 @@ fun buildDummyDivTag(vararg attributes: Pair<String, String?>) = buildString {
     }
     append(" >")
 }
+
+private val jsStringMatcher = PlatformPatterns
+    .psiElement(LeafPsiElement::class.java)
+    .withParent(JSLiteralExpression::class.java)
+
+fun isJsString(element: PsiElement) = jsStringMatcher.accepts(element)

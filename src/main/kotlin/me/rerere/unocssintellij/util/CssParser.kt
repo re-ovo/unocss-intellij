d09b0cabe9d37c89e46b5fbcb8b33a696111bd63
re-ovo <qq1609403959@outlook.com>
2024-01-06 13:02:48 +0800

    fix/perf: 修复透明度解析，并且添加解析缓存

--- src/main/kotlin/me/rerere/unocssintellij/util/CssParser.kt
+++ src/main/kotlin/me/rerere/unocssintellij/util/CssParser.kt
@@ -1,5 +1,6 @@
 package me.rerere.unocssintellij.util
 
+import com.google.common.cache.CacheBuilder
 import com.intellij.lang.css.CSSLanguage
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiElement
@@ -13,8 +14,14 @@ import com.intellij.psi.util.elementType
 import com.intellij.ui.JBColor
 import java.awt.Color
 
+private val parseCache = CacheBuilder.newBuilder()
+    .maximumSize(64)
+    .build<String, Set<JBColor>>()
+
 private val CSS_RGBA_COLOR_PATTERN =
     """rgba\(\d{1,3},\s*\d{1,3},\s*\d{1,3},\s*var\(--[a-z-]+\)\)|rgba\(\d{1,3},\s*\d{1,3},\s*\d{1,3},\s*0\.\d+\)|rgb\(\d{1,3}\s*\d{1,3}\s*\d{1,3}\s*/\s*var\(--[a-z-]+\)\)""".toRegex()
+private val CSS_VAR_DEF_PATTERN =
+    """--[\w-]+:\s*\d+(\.\d+)?;""".toRegex()
 
 private val CSS_COMMENT_PATTERN = """/\*.*?\*/""".toRegex()
 private val CSS_CONTENT_PATTERN = """(?<=\{)[^}]*(?=})""".toRegex()
@@ -29,20 +36,39 @@ private val CSS_HEX_COLOR_RE = "^#([0-9a-fA-F]{2})([0-9a-fA-F]{2})([0-9a-fA-F]{2
 // pattern: rgba(255, 255, 255, 0.5)
 // pattern: rgb(219 39 119 / var(--un-bg-opacity))
 fun parseColors(css: String): Set<JBColor> {
+    if (parseCache.getIfPresent(css) != null) {
+        // Parse color is slow, so we use cache
+        return parseCache.getIfPresent(css) ?: emptySet()
+    }
+
     val colors = mutableSetOf<JBColor>()
+    val varDefs = CSS_VAR_DEF_PATTERN.findAll(css)
+        .map {
+            val varName = it.value.substringBefore(":").trim()
+            val varValue = it.value.removeSuffix(";").substringAfter(":").trim().toFloatOrNull() ?: 1f
+            varName to varValue
+        }
+        .toMap()
+
     CSS_RGBA_COLOR_PATTERN.findAll(css).forEach { matchResult ->
         val value = matchResult.value.removePrefix("rgba(").removePrefix("rgb(").removeSuffix(")")
         val parts = if (value.contains("/")) {
-            value.split(" ", "/")
+            value.split(" ", "/").filter { it.isNotBlank() }
         } else {
             value.split(",")
         }
         val (r, g, b) = parts.map { it.trim().toIntOrNull() ?: 1 }
-        val alphaPart = parts.getOrNull(3) ?: parts.getOrNull(2)?.split("/")?.getOrNull(1)
-        val alpha = ((alphaPart?.trim()?.toFloatOrNull() ?: 1f).coerceIn(0f, 1f) * 255).toInt()
+        val alphaPart = parts.getOrNull(3) ?: parts.getOrNull(2)?.split("/")?.getOrNull(1) ?: ""
+        val alpha = if (alphaPart.startsWith("var(")) {
+            val varName = alphaPart.removePrefix("var(").removeSuffix(")").trim()
+            ((varDefs[varName] ?: 1f).coerceIn(0f, 1f) * 255).toInt()
+        } else {
+            ((alphaPart.trim().toFloatOrNull() ?: 1f).coerceIn(0f, 1f) * 255).toInt()
+        }
         val jbColor = JBColor(Color(r, g, b, alpha), Color(r, g, b, alpha))
         colors.add(jbColor)
     }
+    parseCache.put(css, colors)
     return colors
 }
 

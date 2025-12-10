f33b3ff6e5af2627739416913a70e0b5d75e7f8b
re-ovo <1609403959@qq.com>
2025-12-10 11:34:47 +0800

    build: 更新js依赖

--- src/main/javascript/rollup.config.js
+++ src/main/javascript/rollup.config.js
@@ -2,6 +2,7 @@ import typescript from "@rollup/plugin-typescript";
 import babel from "@rollup/plugin-babel";
 import nodeResolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
+import json from "@rollup/plugin-json";
 
 export default {
     input: 'src/service.ts',
@@ -12,6 +13,7 @@ export default {
         entryFileNames: '[name].mjs',
     },
     plugins: [
+        json(),
         typescript(),
         nodeResolve(),
         commonjs(),
